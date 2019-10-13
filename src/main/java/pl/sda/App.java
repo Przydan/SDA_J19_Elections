040d7738e3446941ee348df7fe11e6e1ef64f73a
Przydan <patryk@przydanek.com>
2019-10-13 09:42:23 +0200

    test init

--- src/main/java/pl/sda/App.java
+++ src/main/java/pl/sda/App.java
@@ -2,12 +2,10 @@ package pl.sda;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("test");
     }
 }
